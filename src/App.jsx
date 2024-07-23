663a792b97cb523e369e691ab77482d6c3a1fd9d
oscarter19 <oscarg1910@gmail.com>
2024-07-23 18:47:18 -0400

    Primer codigo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <header>
+    <h1 className='title'>🎥Peliculas</h1>
+   </header>
+  )
+}
+
+export default App
